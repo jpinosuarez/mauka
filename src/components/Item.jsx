0c593628fc7b1921b02c5164fa58cd4405095907
jpinosuarez <pinosuarezjoaquin@gmail.com>
2022-04-04 20:28:13 -0300

    cambios

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Item = (item) => {
     return (
-        <div className='card d-flex flex-column align-items-center mx-2'>
+        <div className='card d-flex flex-column align-items-center mx-2 '>
             <img className='w-75' src={item.pictureUrl} alt="" />
             <p className='text-center'>{item.title}</p>
             <p className='text-center'>$ {item.price}</p>
-            <button className='btn btn-primary'>Ver detalles</button>
+            <button className='btn btn-primary my-2'>Ver detalles</button>
         </div>
     )
 }
