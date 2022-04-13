ad986704c528b1c500bc007431b5044ee200ded1
jpinosuarez <pinosuarezjoaquin@gmail.com>
2022-04-12 21:08:22 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ItemDetailContainer from './containers/ItemDetail/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cart from './components/Cart/Cart';
 import Error404 from './components/404/Error404';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -11,13 +12,18 @@ function App() {
       <header>
         <NavBar />
       </header>
-    <Routes>
-      <Route path="/" element={<ItemListContainer/>}/>
-      <Route path="/category/:id" element={<ItemListContainer/>}/>
-      <Route path="/item/:id" element={<ItemDetailContainer/>}/>
-      <Route path="/cart" element={<Cart/>}/>
-      <Route path="*" element={<Error404/>}/>
-    </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:id" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </main>
+      {/* <footer>
+        <Footer/>
+      </footer> */}
     </BrowserRouter>
   )
 }
