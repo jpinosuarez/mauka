573b6039f5b3e407314dc8f6e046e861fce454dc
Joaquín Pino Suárez <pinosuarezjoaquin@gmail.com>
2022-04-21 18:01:48 -0300

    cart styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import CartItem from './CartItem';
 import { contexto } from '../../context/CartContext';
+import "../../styles/main.css"
 
 
 const Cart = () => {
@@ -18,10 +19,33 @@ const Cart = () => {
 
 
     return (
-        cart.map((product) => {
-            return <CartItem title={product.title} price={product.price} />
-        })
+        <>
+
+            <div id='cart' >
+                <h1>Carrito de compras</h1>
+                <div className='d-flex flex-row justify-content-around'>
+                    <div className='w-75 px-5'>
+                        <ul className='d-flex flex-row justify-content-around'>
+                            <li className='w-75'>Item</li>
+                            <div className='w-25 d-flex flex-row justify-content-around'>
+                                <li>Precio</li>
+                                <li>Cantidad</li>
+                                <li>Subtotal</li>
+                            </div>
+                        </ul>
+                        {cart.map((product) => {
+                            return <CartItem title={product.title} price={product.price} pictureURL={product.pictureUrl} />
+                        })}
+                    </div>
+                    <div className='w-25 px-5'>
+                        <h2>Resumen</h2>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
+
 }
 
 export default Cart;
--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import "../../styles/main.css"
 
 const CartItem = (product) => {
 
     return (
-        <div className='d-flex flex-row'>
-            <h2>{product.title}</h2>
-            <p>{product.price}</p>
+        <div id='CartItem' className='d-flex flex-row'>
+            <div className='w-75 d-flex flex-row align-items-center'>
+                <img src={product.pictureURL} alt="" />
+                <p>{product.title}</p>
+            </div>
+            <div className='w-25 d-flex flex-row justify-content-around'>
+                <p>${product.price}</p>
+                <p>qty</p>
+                <p>subtotal</p>
+            </div>
         </div>
     )
 }
